d69674ab327ed7d36d11d2440c053c56e5a5868b
JerryLiu <frencyliu@gmail.com>
2022-10-12 00:46:13 +0800

    remove function

--- functions.php
+++ functions.php
@@ -1,11 +1,2 @@
 <?php
 
-if (!function_exists('cleanSpecialChar')) {
-    function cleanSpecialChar($string)
-    {
-        $string = preg_replace('/[^A-Za-z0-9\-]/', ' ', $string);
-        $string = preg_replace('/-+/', ' ', $string);
-        $string =  preg_replace('/\s+/', ' ', $string);
-        return $string;
-    }
-}
